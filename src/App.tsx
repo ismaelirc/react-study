7da3815882686c78228f66b826d7436ad61e8f79
Ismael <ismaelirc@gmail.com>
2022-05-24 17:35:34 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+interface ButtonProps{
+	text?: String
+}
+
+function Button(props: ButtonProps){
+	return <button className="bg-violet-500 p-2 rounded hover:bg-violet-700 transition-colors">{props.text ?? "Default"}</button>
+}
+
+function App() {
+
+	return (
+		<div className="flex gap-2">
+			<Button text="Enviar" />
+			<Button text="ok"/>
+			<Button />
+			
+		</div>
+	)
+}
+
+export default App
